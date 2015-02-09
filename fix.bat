4d98b3167e26ea125c092ac49fd4da815a95d261
Danny Fredrick <df@cryptostorm.is>
2015-02-09 14:08:56 -0600

    Update fix.bat

--- fix.bat
+++ fix.bat
@@ -2,18 +2,7 @@
 ::save as a .bat file, run as administrator
 ::then visit https://diafygi.github.io/webrtc-ips/ to verify
 ::no more public IP leaking :-D
-netsh advfirewall firewall add rule name="No STUN leak for j00!" dir=out action=block remoteip=107.23.150.92 protocol=any
-netsh advfirewall firewall add rule name="No STUN leak for j00!" dir=out action=block remoteip=54.172.47.69 protocol=any
-netsh advfirewall firewall add rule name="No STUN leak for j00!" dir=out action=block remoteip=173.194.65.127 protocol=any
-netsh advfirewall firewall add rule name="No STUN leak for j00!" dir=out action=block remoteip=173.194.67.127 protocol=any
-netsh advfirewall firewall add rule name="No STUN leak for j00!" dir=out action=block remoteip=173.194.68.127 protocol=any
-netsh advfirewall firewall add rule name="No STUN leak for j00!" dir=out action=block remoteip=173.194.71.127 protocol=any
-netsh advfirewall firewall add rule name="No STUN leak for j00!" dir=out action=block remoteip=173.194.72.127 protocol=any
-netsh advfirewall firewall add rule name="No STUN leak for j00!" dir=out action=block remoteip=173.194.73.127 protocol=any
-netsh advfirewall firewall add rule name="No STUN leak for j00!" dir=out action=block remoteip=173.194.78.127 protocol=any
-netsh advfirewall firewall add rule name="No STUN leak for j00!" dir=out action=block remoteip=74.125.131.127 protocol=any
-netsh advfirewall firewall add rule name="No STUN leak for j00!" dir=out action=block remoteip=74.125.136.127 protocol=any
-netsh advfirewall firewall add rule name="No STUN leak for j00!" dir=out action=block remoteip=74.125.142.127 protocol=any
-netsh advfirewall firewall add rule name="No STUN leak for j00!" dir=out action=block remoteip=74.125.200.127 protocol=any
-::to disable the above use: (without the :: at the beginning)
-::netsh advfirewall firewall del rule name="No STUN leak for j00!"
+netsh advfirewall firewall add rule name="No STUN leak for j00!" dir=out action=block protocol=UDP localport=3478
+netsh advfirewall firewall add rule name="No STUN leak for j00!" dir=out action=block protocol=UDP remoteport=3478
+netsh advfirewall firewall add rule name="No STUN leak for j00!" dir=in action=block protocol=UDP localport=3478
+netsh advfirewall firewall add rule name="No STUN leak for j00!" dir=in action=block protocol=UDP remoteport=3478
